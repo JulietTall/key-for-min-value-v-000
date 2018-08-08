7cb8253accddcde452eb1d1643cf3b7438453a56
Juliet Tallmer <juliettallmer@icloud.com>
2018-08-08 21:06:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
    lowest_value = nil
-   hash.each do |key, value|
-     if lowest_value == nil || value < lowest_value
-       lowest_value = value
-       lowest_key = key
+   hash.each do |k, v|
+     if lowest_value == nil || v < lowest_value
+       lowest_value = v
+       lowest_key = k
      end
    end
    lowest_key
